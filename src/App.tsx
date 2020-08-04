1e51d30fb8d7507a9148f2565bd3f4ed2712296d
bradleykenny <bkenny283@gmail.com>
2020-08-04 11:10:07 +1000

    Setting up template for login/register files.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style/App.css";
+
+const App = () => {
+	return (
+		<div className="App">
+			<p>
+				Edit <code>src/App.js</code> and save to reload.
+			</p>
+		</div>
+	);
+};
+
+export default App;
