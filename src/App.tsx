69e5937213de5af1e59ac6bf632355313f12a0af
George Tsamoglou <george.gt39@gmail.com>
2020-08-11 10:51:48 +1000

    made navbar into a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style/App.css";
 import 'bootstrap/dist/css/bootstrap.css'; 
-import { Nav, Navbar, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+import NavBar from './components/NavBar';
 
-const App = () => {
-	return (
-		<div className="App">
-			<Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
-				<Navbar.Brand href="#home">Favour Town</Navbar.Brand>
-				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-				<Navbar.Collapse id="responsive-navbar-nav">
-				<Nav className="mr-auto">
-					<Nav.Link href="#features">Friends</Nav.Link>
-					<Nav.Link href="#pricing">Favors</Nav.Link>
-					<NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-					<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-					<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-					<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-					<NavDropdown.Divider />
-					<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-					</NavDropdown>
-				</Nav>
-				<Form inline>
-					<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-					<Button variant="outline-light">Search</Button>
-				</Form>
-				</Navbar.Collapse>
-			</Navbar>
-			<p>
-				Edit <code>src/App.js</code> and save to reload.
-			</p>
-		</div>
-	);
-};
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<NavBar />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+			</div>
+		); 
+	}
+}
 
 export default App;
