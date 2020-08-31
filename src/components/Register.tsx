aab6ec59814afc0fa0cb7e2af76761053a91f596
George Tsamoglou <george.gt39@gmail.com>
2020-08-31 23:51:05 +1000

    styling register component to match login component

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -116,94 +116,105 @@ export const Register = (props: RegisterProps) => {
 	};
 
 	return (
-		<Container>
-			<Row className="d-flex justify-content-center">
-				<Col sm={4} className="align-items-center">
-					<Card bg="light">
-						<Card.Body>
-							<Card.Title className="text-center">
-								<h1>Create Account</h1>
-							</Card.Title>
-							<Form onSubmit={handleSubmit}>
-								<Form.Group controlId="formUsername">
-									<Form.Label>Username</Form.Label>
-									<Form.Control
-										name="username"
-										type="username"
-										placeholder="Username"
-										onChange={handleChange}
-									/>
-									{errors.username.length > 0 && (
-										<Form.Text className="error">
-											{errors.username}
-										</Form.Text>
-									)}
-								</Form.Group>
+		<div
+			style={{
+				backgroundImage:
+					"url(https://www.xmple.com/wallpaper/blue-purple-linear-gradient-1920x1080-c2-ee82ee-40e0d0-a-15-f-14.svg)",
+				height: "100%",
+				width: "100%",
+			}}
+		>
+			<Container className="align-items-center">
+				<Row className="d-flex justify-content-center">
+					<Col sm={4} className="align-items-center">
+						<Card bg="light">
+							<Card.Body>
+								<Card.Title className="text-center">
+									<h1>Create Account</h1>
+								</Card.Title>
+								<Form onSubmit={handleSubmit}>
+									<Form.Group controlId="formUsername">
+										<Form.Label>Username</Form.Label>
+										<Form.Control
+											name="username"
+											type="username"
+											placeholder="Username"
+											onChange={handleChange}
+										/>
+										{errors.username.length > 0 && (
+											<Form.Text className="error">
+												{errors.username}
+											</Form.Text>
+										)}
+									</Form.Group>
 
-								<Form.Group controlId="formEmail">
-									<Form.Label>Email</Form.Label>
-									<Form.Control
-										name="email"
-										type="email"
-										placeholder="Email"
-										onChange={handleChange}
-									/>
-									{errors.email.length > 0 && (
-										<Form.Text className="error">
-											{errors.email}
-										</Form.Text>
-									)}
-								</Form.Group>
+									<Form.Group controlId="formEmail">
+										<Form.Label>Email</Form.Label>
+										<Form.Control
+											name="email"
+											type="email"
+											placeholder="Email"
+											onChange={handleChange}
+										/>
+										{errors.email.length > 0 && (
+											<Form.Text className="error">
+												{errors.email}
+											</Form.Text>
+										)}
+									</Form.Group>
 
-								<Form.Group controlId="formPassword">
-									<Form.Label>Password</Form.Label>
-									<Form.Control
-										name="password"
-										type="password"
-										placeholder="Password"
-										onChange={handleChange}
-									/>
-									{errors.password.length > 0 && (
-										<Form.Text className="error">
-											{errors.password}
-										</Form.Text>
-									)}
-								</Form.Group>
+									<Form.Group controlId="formPassword">
+										<Form.Label>Password</Form.Label>
+										<Form.Control
+											name="password"
+											type="password"
+											placeholder="Password"
+											onChange={handleChange}
+										/>
+										{errors.password.length > 0 && (
+											<Form.Text className="error">
+												{errors.password}
+											</Form.Text>
+										)}
+									</Form.Group>
 
-								<Form.Group controlId="formConfirmPassword">
-									<Form.Label>Confirm Password</Form.Label>
-									<Form.Control
-										name="confirmPassword"
-										type="password"
-										placeholder="Confirm Password"
-										onChange={handleChange}
-									/>
-									{errors.confirmPassword.length > 0 && (
-										<Form.Text className="error">
-											{errors.confirmPassword}
-										</Form.Text>
-									)}
-								</Form.Group>
-								<Button
-									variant="primary"
-									type="submit"
-									className="btn-block"
-								>
-									Register
-								</Button>
-							</Form>
-						</Card.Body>
-						<Card.Footer>
-							<p className="font-small grey-text d-flex justify-content-end">
-								Already have an account?
-								<a href="/login" className="blue-text ml-1">
-									Login
-								</a>
-							</p>
-						</Card.Footer>
-					</Card>
-				</Col>
-			</Row>
-		</Container>
+									<Form.Group controlId="formConfirmPassword">
+										<Form.Label>
+											Confirm Password
+										</Form.Label>
+										<Form.Control
+											name="confirmPassword"
+											type="password"
+											placeholder="Confirm Password"
+											onChange={handleChange}
+										/>
+										{errors.confirmPassword.length > 0 && (
+											<Form.Text className="error">
+												{errors.confirmPassword}
+											</Form.Text>
+										)}
+									</Form.Group>
+									<Button
+										variant="primary"
+										type="submit"
+										className="btn-block"
+									>
+										Register
+									</Button>
+								</Form>
+							</Card.Body>
+							<Card.Footer>
+								<p className="font-small grey-text d-flex justify-content-end">
+									Already have an account?
+									<a href="/login" className="blue-text ml-1">
+										Login
+									</a>
+								</p>
+							</Card.Footer>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+		</div>
 	);
 };
