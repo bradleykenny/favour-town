1cbb99c58911ab9cba0df3d8b9ff511772822d90
bradleykenny <bkenny283@gmail.com>
2020-09-01 10:53:56 +1000

    Reading cards from API.

--- src/components/FeedList.tsx
+++ src/components/FeedList.tsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { FeedCard } from "./FeedCard";
+import axios from "axios";
 
 import "../style/FeedList.css";
 
 type FeedListProps = {};
 
+type Favour = {
+	_id: string;
+	user_id: string;
+	title: string;
+	location: string;
+	description: string;
+	favour_coins: number;
+	favour_type: number;
+	date: string;
+};
+
 export const FeedList = (props: FeedListProps) => {
+	const defaultFavour: Favour = {
+		_id: "",
+		user_id: "",
+		title: "",
+		location: "",
+		description: "",
+		favour_coins: 0,
+		favour_type: 0,
+		date: "",
+	};
+	const [cards, setCards] = useState([defaultFavour]);
+
+	useEffect(() => {
+		axios.get("http://localhost:5000/favours").then((response) => {
+			setCards(response.data);
+		});
+	}, []);
+
 	return (
 		<div id="feedList">
-			<FeedCard
-				title="Example Card"
-				text="Some example text"
-				username="bradknny"
-			/>
-			<FeedCard
-				title="Example Card"
-				text="Some example text"
-				username="bradknny"
-			/>
+			{cards.map((favour: Favour) => (
+				<FeedCard
+					title={favour.title}
+					text={favour.description}
+					username={favour.user_id}
+				/>
+			))}
 		</div>
 	);
 };
