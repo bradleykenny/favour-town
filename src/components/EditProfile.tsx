0111badca1f4a4cfa9f3cd94589faabe4c6607fc
George Tsamoglou <george.gt39@gmail.com>
2020-10-19 21:39:08 +1100

    star rating UI

--- src/components/EditProfile.tsx
+++ src/components/EditProfile.tsx
@@ -21,7 +21,8 @@ type EditProfileProps = {
 
 export const EditProfile = (props: EditProfileProps) => {
 	const { username } = useParams<{ username: string }>();
-	const [profilePic, setProfilePic] = useState("")
+	const [profilePic, setProfilePic] = useState("");
+	const starRating = 3;
 
 	const blankUser: ProfileType = {
 		username: "",
@@ -45,8 +46,8 @@ export const EditProfile = (props: EditProfileProps) => {
 	const handleSubmit = () => {};
 
 	const handleChange = (e: any) => {
-		setProfilePic(e.target.files[0])
-		console.log(e.target.files[0])
+		setProfilePic(e.target.files[0]);
+		console.log(e.target.files[0]);
 	};
 
 	return (
@@ -59,6 +60,38 @@ export const EditProfile = (props: EditProfileProps) => {
 				/>
 				<h1>@{props.user.username}</h1>
 				<h3>{props.user.email_addr}</h3>
+				<div className="rating">
+					{[...Array(5)].map((star, i) => {
+						const ratingValue = i + 1;
+
+						return (
+							<label>
+								<input
+									type="radio"
+									name="rating"
+									value={ratingValue}
+								/>
+								<svg
+									width="1em"
+									height="1em"
+									viewBox="0 0 16 16"
+									className="bi bi-star-fill"
+									fill={
+										ratingValue <= starRating
+											? "#ffc107"
+											: "#d4d5d9"
+									}
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+								</svg>
+							</label>
+						);
+					})}
+					<i className="text-muted ml-2">
+						Your rating is: {starRating}
+					</i>
+				</div>
 			</Jumbotron>
 			<Container>
 				<Row className="d-flex justify-content-center">
