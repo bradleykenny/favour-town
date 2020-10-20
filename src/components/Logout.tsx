8294a525e261ff034e88e93c09dd2e7a3afe32ba
bradleykenny <bkenny283@gmail.com>
2020-10-20 11:00:55 +1100

    Formatting.

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,16 +1,21 @@
 import axios from "axios";
 
 export const Logout = (e: any) => {
-    axios.post(process.env.REACT_APP_API_HOST + "/logout",{},{withCredentials:true}).then(
-        (response) => {
-            console.log(response)
-            if(response.data==="OK"){
-                window.location.assign("/")
-                window.alert("Logout Succesful. Please Login Again.")
-            }
-        },
-        (error) => {
-            console.log(error);
-        }
-    )
+	axios
+		.post(
+			process.env.REACT_APP_API_HOST + "/logout",
+			{},
+			{ withCredentials: true }
+		)
+		.then(
+			(response) => {
+				if (response.data === "OK") {
+					window.location.assign("/");
+					window.alert("Logout Succesful. Please Login Again.");
+				}
+			},
+			(error) => {
+				console.error(error);
+			}
+		);
 };
