971002481dd5ae5858da01b99c5ee5d6b9a4740e
George Tsamoglou <george.gt39@gmail.com>
2020-10-27 00:06:50 +1100

    fixes to friend and chatmessage components

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -15,6 +15,7 @@ import "../style/DirectMessage.css";
 
 type messageProps = {
 	message: {
+		authorId: 0;
 		author: string;
 		avatar: string;
 		when: string;
@@ -22,27 +23,29 @@ type messageProps = {
 	};
 };
 
-export const ChatMessage = (props: messageProps) => (
-	<li className="chat-message d-flex justify-content-between mb-4">
-		<img
-			className="img-circle mr-2 z-depth-1"
-			src={props.message.avatar}
-			alt="avatar"
-			style={{ width: "3rem", height: "3rem" }}
-		/>
-		<Card>
-			<Card.Body>
-				<div>
-					<strong className="primary-font">
-						{props.message.author}
-					</strong>
-					<small className="pull-right text-muted">
-						<i className="far fa-clock" /> {props.message.when}
-					</small>
-				</div>
-				<hr />
-				<p className="mb-0">{props.message.message}</p>
-			</Card.Body>
-		</Card>
-	</li>
-);
+export const ChatMessage = (props: messageProps) => {
+	return (
+		<li className="chat-message d-flex justify-content-between mb-4">
+			<img
+				className="img-circle mr-2 z-depth-1"
+				src={props.message.avatar}
+				alt="avatar"
+				style={{ width: "3rem", height: "3rem" }}
+			/>
+			<Card>
+				<Card.Body>
+					<div>
+						<strong className="primary-font">
+							{props.message.author}
+						</strong>
+						<small className="pull-right text-muted">
+							<i className="far fa-clock" /> {props.message.when}
+						</small>
+					</div>
+					<hr />
+					<p className="mb-0">{props.message.message}</p>
+				</Card.Body>
+			</Card>
+		</li>
+	);
+};
--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -15,6 +15,7 @@ import "../style/DirectMessage.css";
 
 type friendProps = {
 	friend: {
+		friendId: 0;
 		name: string;
 		avatar: string;
 		message: string;
@@ -24,62 +25,69 @@ type friendProps = {
 		active: boolean;
 	};
 };
-export const Friend = (props: friendProps) => (
-	<ListGroupItem
-		href="#!"
-		className="d-flex justify-content-between p-2
+export const Friend = (props: friendProps) => {
+	const handleClick = (e: any) => {
+		e.preventDefault();
+		alert(props.friend.name);
+	};
+	return (
+		<ListGroupItem
+			href="#!"
+			className="d-flex justify-content-between p-2
         border-light"
-		style={{ backgroundColor: props.friend.active ? "#eeeeee" : "" }}
-	>
-		<img
-			className="img-circle mr-2 z-depth-1"
-			src={props.friend.avatar}
-			alt="avatar"
-			style={{ width: "3em", height: "3rem" }}
-		/>
-		<div style={{ fontSize: "0.95rem" }}>
-			<strong>{props.friend.name}</strong>
-			<p className="text-muted">{props.friend.message}</p>
-		</div>
-		<div>
-			<p className="text-muted mb-0" style={{ fontSize: "0.75rem" }}>
-				{props.friend.when}
-			</p>
-			{props.friend.seen ? (
-				<span className="text-muted float-right">
-					<svg
-						width="1em"
-						height="1em"
-						viewBox="0 0 16 16"
-						className="bi bi-check"
-						fill="currentColor"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"
-						/>
-					</svg>
-				</span>
-			) : props.friend.toRespond ? (
-				<Badge color="danger" className="float-right">
-					{props.friend.toRespond}
-				</Badge>
-			) : (
-				<span className="text-muted float-right">
-					<svg
-						width="1em"
-						height="1em"
-						viewBox="0 0 16 16"
-						className="bi bi-reply-fill"
-						id="replyIcon"
-						fill="currentColor"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path d="M9.079 11.9l4.568-3.281a.719.719 0 0 0 0-1.238L9.079 4.1A.716.716 0 0 0 8 4.719V6c-1.5 0-6 0-7 8 2.5-4.5 7-4 7-4v1.281c0 .56.606.898 1.079.62z" />
-					</svg>
-				</span>
-			)}
-		</div>
-	</ListGroupItem>
-);
+			style={{ backgroundColor: props.friend.active ? "#eeeeee" : "" }}
+			onClick={handleClick}
+		>
+			<img
+				className="img-circle mr-2 z-depth-1"
+				src={props.friend.avatar}
+				alt="avatar"
+				style={{ width: "3em", height: "3rem" }}
+			/>
+			<div style={{ fontSize: "0.95rem" }}>
+				<strong>{props.friend.name}</strong>
+				<p className="text-muted">{props.friend.message}</p>
+			</div>
+			<div>
+				<p className="text-muted mb-0" style={{ fontSize: "0.75rem" }}>
+					{props.friend.when}
+				</p>
+				{props.friend.seen ? (
+					<span className="text-muted float-right">
+						<svg
+							width="1em"
+							height="1em"
+							viewBox="0 0 16 16"
+							className="bi bi-check"
+							fill="currentColor"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"
+							/>
+						</svg>
+					</span>
+				) : props.friend.toRespond ? (
+					<Badge color="danger" className="float-right">
+						{props.friend.toRespond}
+					</Badge>
+				) : (
+					<span className="text-muted float-right">
+						<svg
+							width="1em"
+							height="1em"
+							viewBox="0 0 16 16"
+							className="bi bi-reply-fill"
+							id="replyIcon"
+							fill="currentColor"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path d="M9.079 11.9l4.568-3.281a.719.719 0 0 0 0-1.238L9.079 4.1A.716.716 0 0 0 8 4.719V6c-1.5 0-6 0-7 8 2.5-4.5 7-4 7-4v1.281c0 .56.606.898 1.079.62z" />
+						</svg>
+					</span>
+				)}
+			</div>
+		</ListGroupItem>
+	);
+};
