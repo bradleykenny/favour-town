e375af5c14bca7ef8a5f600b22ac50c042278203
George Tsamoglou <george.gt39@gmail.com>
2020-10-26 23:17:04 +1100

    new messages are shown in the message feed

--- src/components/DirectMessage.tsx
+++ src/components/DirectMessage.tsx
@@ -17,130 +17,125 @@ import { ChatMessage, Friend } from "../components";
 import "../style/DirectMessage.css";
 type messageProps = {};
 const socket = socketIOClient("http://localhost:5000"); //public is the room name
-socket.on("NotLoggedIn",(msg:string)=>{
+socket.on("NotLoggedIn", (msg: string) => {
 	console.log(msg);
 	//Redirect to login page
-})
+});
 
-socket.on("ACK",(msg:string)=>{
+socket.on("ACK", (msg: string) => {
 	console.log(msg);
-})
+});
 
-socket.on("incoming",(msgList:object[])=>{
+socket.on("incoming", (msgList: object[]) => {
 	//Update message list state with list of messages
-})
+});
 
-socket.on("friendslist",(friends:object[])=>{
-	console.log(friends)
+socket.on("friendslist", (friends: object[]) => {
+	console.log(friends);
 	//Update list of friends (i.e. people you have recieved messages from or sent messages to). Each object will contain the user_id, username and the last message recieved from them
-})
+});
 
 export const DirectMessage = (props: messageProps) => {
 	const [newMessage, setNewMessage] = useState("");
-	const [state, setState] = useState({
-		friends: [
-			{
-				name: "John Doe",
-				avatar: "https://robohash.org/John",
-				message: "Hello, Are you there?",
-				when: "Just now",
-				toRespond: 1,
-				seen: false,
-				active: true,
-			},
-			{
-				name: "Danny Smith",
-				message: "Lorem ipsum dolor sit",
-				avatar: "https://robohash.org/Danny",
-				when: "5 min ago",
-				toRespond: 0,
-				seen: false,
-				active: false,
-			},
-			{
-				name: "Alex Steward",
-				message: "Lorem ipsum dolor sit",
-				avatar: "https://robohash.org/Alex",
-				when: "Yesterday",
-				toRespond: 0,
-				seen: false,
-				active: false,
-			},
-			{
-				name: "Ashley Olsen",
-				message: "Lorem ipsum dolor sit",
-				avatar: "https://robohash.org/Ashley",
-				when: "Yesterday",
-				toRespond: 0,
-				seen: false,
-				active: false,
-			},
-			{
-				name: "Kate Moss",
-				message: "Lorem ipsum dolor sit",
-				avatar: "https://robohash.org/Kate",
-				when: "Yesterday",
-				toRespond: 0,
-				seen: false,
-				active: false,
-			},
-			{
-				name: "Lara Croft",
-				message: "Lorem ipsum dolor sit",
-				avatar: "https://robohash.org/Lara",
-				when: "Yesterday",
-				toRespond: 0,
-				seen: false,
-				active: false,
-			},
-			{
-				name: "Brad Pitt",
-				message: "Lorem ipsum dolor sit",
-				avatar: "https://robohash.org/Brad",
-				when: "5 min ago",
-				toRespond: 0,
-				seen: true,
-				active: false,
-			},
-		],
-		messages: [
-			{
-				author: "Brad Pitt",
-				avatar: "https://robohash.org/Brad",
-				when: "12 mins ago",
-				message:
-					"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolo",
-			},
-			{
-				author: "Lara Croft",
-				avatar: "https://robohash.org/Lara",
-				when: "13 mins ago",
-				message:
-					" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
-			},
-			{
-				author: "Brad Pitt",
-				avatar: "https://robohash.org/Brad",
-				when: "14 mins ago",
-				message:
-					"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dol",
-			},
-		],
-	});
+	const [friends, setFriends] = useState([
+		{
+			name: "John Doe",
+			avatar: "https://robohash.org/John",
+			message: "Hello, Are you there?",
+			when: "Just now",
+			toRespond: 1,
+			seen: false,
+			active: true,
+		},
+		{
+			name: "Danny Smith",
+			message: "Lorem ipsum dolor sit",
+			avatar: "https://robohash.org/Danny",
+			when: "5 min ago",
+			toRespond: 0,
+			seen: false,
+			active: false,
+		},
+		{
+			name: "Alex Steward",
+			message: "Lorem ipsum dolor sit",
+			avatar: "https://robohash.org/Alex",
+			when: "Yesterday",
+			toRespond: 0,
+			seen: false,
+			active: false,
+		},
+		{
+			name: "Ashley Olsen",
+			message: "Lorem ipsum dolor sit",
+			avatar: "https://robohash.org/Ashley",
+			when: "Yesterday",
+			toRespond: 0,
+			seen: false,
+			active: false,
+		},
+		{
+			name: "Kate Moss",
+			message: "Lorem ipsum dolor sit",
+			avatar: "https://robohash.org/Kate",
+			when: "Yesterday",
+			toRespond: 0,
+			seen: false,
+			active: false,
+		},
+		{
+			name: "Lara Croft",
+			message: "Lorem ipsum dolor sit",
+			avatar: "https://robohash.org/Lara",
+			when: "Yesterday",
+			toRespond: 0,
+			seen: false,
+			active: false,
+		},
+		{
+			name: "Brad Pitt",
+			message: "Lorem ipsum dolor sit",
+			avatar: "https://robohash.org/Brad",
+			when: "5 min ago",
+			toRespond: 0,
+			seen: true,
+			active: false,
+		},
+	]);
+	const [messages, setMessages] = useState([
+		{
+			author: "Brad Pitt",
+			avatar: "https://robohash.org/Brad",
+			when: "12 mins ago",
+			message:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolo",
+		},
+		{
+			author: "Lara Croft",
+			avatar: "https://robohash.org/Lara",
+			when: "13 mins ago",
+			message:
+				" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+		},
+		{
+			author: "Brad Pitt",
+			avatar: "https://robohash.org/Brad",
+			when: "14 mins ago",
+			message:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dol",
+		},
+	]);
 
 	const handleSubmit = (e: any) => {
 		e.preventDefault();
 		console.log(newMessage);
-		// setState({
-		// 	messages: [
-		// 		{
-		// 			author: "Lara Croft",
-		// 			avatar: "https://robohash.org/Lara",
-		// 			when: "13 mins ago",
-		// 			message: newMessage,
-		// 		},
-		// 	],
-		// });
+		const message = {
+			author: "Lara Croft",
+			avatar: "https://robohash.org/Lara",
+			when: "13 mins ago",
+			message: newMessage,
+		};
+		setMessages([...messages, message]);
 	};
 
 	const handleChange = (e: any) => {
@@ -157,7 +152,7 @@ export const DirectMessage = (props: messageProps) => {
 						<h3 className="font-weight-bold mb-2 ml-3">Member</h3>
 						<div className="white z-depth-1 p-3">
 							<ListGroup className="friend-list">
-								{state.friends.map((friend: any) => (
+								{friends.map((friend: any) => (
 									<Friend key={friend.name} friend={friend} />
 								))}
 							</ListGroup>
@@ -166,7 +161,7 @@ export const DirectMessage = (props: messageProps) => {
 					<Col>
 						<Row>
 							<ListGroup>
-								{state.messages.map((message: any) => (
+								{messages.map((message: any) => (
 									<ChatMessage
 										key={message.author + message.when}
 										message={message}
