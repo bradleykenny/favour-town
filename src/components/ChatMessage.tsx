fa34b3596e481af2ce9e97d28335bad4119ab712
George Tsamoglou <george.gt39@gmail.com>
2020-11-02 18:38:13 +1100

    time formatted using react moment, margin-top for chat message

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Card } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 import "../style/DirectMessage.css";
 
 type messageProps = {
@@ -17,27 +17,30 @@ type messageProps = {
 
 export const ChatMessage = (props: messageProps) => {
 	return (
-		<li className="chat-message d-flex justify-content-between mb-4">
+		<li className="chat-message d-flex justify-content-between mb-4 mt-1">
 			<img
 				className="img-circle mr-2 z-depth-1"
 				src={props.message.avatar}
 				alt="avatar"
 				style={{ width: "3rem", height: "3rem" }}
 			/>
-			<Card className="chatMessageCard">
-				<Card.Body>
-					<div>
-						<strong className="primary-font">
-							{props.message.author}
-						</strong>
-						<small className="pull-right text-muted">
-							<i className="far fa-clock" /> {props.message.when}
-						</small>
-					</div>
-					<hr />
-					<p className="mb-0">{props.message.message}</p>
-				</Card.Body>
-			</Card>
+			<Col>
+				<Card className="chatMessageLeft">
+					<Card.Body>
+						<div>
+							<strong className="primary-font">
+								{props.message.author}
+							</strong>
+							<small className="text-muted">
+								<i className="far fa-clock" />{" "}
+								{props.message.when}
+							</small>
+						</div>
+						<hr />
+						<p className="mb-0">{props.message.message}</p>
+					</Card.Body>
+				</Card>
+			</Col>
 		</li>
 	);
 };
